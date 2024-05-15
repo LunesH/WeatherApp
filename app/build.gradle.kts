4a1f6fb10367ba997b2671352738324daca189c6
jpohl <jpohl@stud.hs-bremen.de>
2024-05-15 11:30:18 +0200

    connection zur openweathermapAPI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -71,4 +72,12 @@ dependencies {
     //navigation
     val navVersion = "2.7.1"
     implementation ("androidx.navigation:navigation-compose:$navVersion")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    val liveDataVersion = "1.3.0-rc01"
+    implementation ("androidx.compose.runtime:runtime-livedata:$liveDataVersion")
+    implementation ("com.squareup.okhttp3:okhttp:4.4.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.4.0")
+
 }
