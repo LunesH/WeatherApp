83f50c1f291346f1421e34644ce8bb510abcc03f
LunesH <105421376+LunesH@users.noreply.github.com>
2024-05-17 18:32:48 +0200

    fix naming

--- app/src/main/java/com/example/weatherapp/viewmodel/PlaceViewmodel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/PlaceViewmodel.kt
@@ -1,11 +1,8 @@
 package com.example.weatherapp.viewmodel
 
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.repository.PlaceRepository
 
